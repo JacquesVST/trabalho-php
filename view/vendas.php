70e594f3a7a5a167a765d259ce5a217f527a8a04
Jacques Trevisan <jacquesvst@gmail.com>
2019-11-09 15:27:17 -0300

    header

--- view/vendas.php
+++ view/vendas.php
@@ -18,26 +18,9 @@ $vendas = $vendaController->recuperarTodas();
     <title>Document</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: purple !important; color: white !important;">
-    <a class="navbar-brand" href="#" style="color: white !important;">Trabalho PHP</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav" style="color: white !important;">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="./produtos.php" style="color: white !important;">Produtos <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="./funcionarios.php" style="color: white !important;">Funcionarios</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="./vendas.php" style="color: white !important;">Vendas</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-
+<?php
+    require_once('header.php');
+?>  
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
